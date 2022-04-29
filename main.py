d2d38dc4efae7bb6112e1e14128b77c8a8657754
Harshita <02.harshy@gmail.com>
2022-04-29 09:46:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello World")
-print("hihihi")
+from math import sqrt
+ 
+def isPrime(n):
+ 
+    if (n <= 1):
+        return False
+   
+    for i in range(2, int(sqrt(n))+1):
+        if (n % i == 0):
+            return False
+ 
+    return True
+ 
+if isPrime(11):
+    print("true")
+else:
+    print("false")

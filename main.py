f7ced9a922964376b98b94cbe3f7c1b634715151
Harshita <02.harshy@gmail.com>
2022-05-02 11:04:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from math import sqrt
- 
-def isPrime(n):
- 
-    if (n <= 1):
-        return False
-   
-    for i in range(2, int(sqrt(n))+1):
-        if (n % i == 0):
-            return False
- 
-    return True
- 
-if isPrime(0):
-    print("true")
-else:
-    print("false")
+print("hi")
